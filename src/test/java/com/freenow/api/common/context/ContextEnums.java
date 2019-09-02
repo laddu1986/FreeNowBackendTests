d21d1a8e523646df047256dd675096b9302e57c0
far11ven <kushalbhalaik@gmail.com>
2019-09-02 21:58:15 +0530

    added TestContext, Log & FileUtils

---
+++ src/test/java/com/freenow/api/common/context/ContextEnums.java
@@ -0,0 +1,9 @@
+package com.freenow.api.common.context;
+
+public enum ContextEnums {
+	TEST_RUN_TIMESTAMP,
+	CURRENT_SCENARIO_ID,
+	CURRENT_SCENARIO_NAME,
+	API_ENDPOINT,
+	RESPONSE
+}
---
+++ src/test/java/com/freenow/api/common/context/TestContext.java
@@ -0,0 +1,46 @@
+package com.freenow.api.common.context;
+
+import com.freenow.api.utils.RestTemplate;
+import com.freenow.global.utils.ConfigReader;
+import com.freenow.global.utils.LogUtils;
+
+/**
+ * Using PicoContainer to share state between stepdefs in a scenario
+ */
+/**
+ * @author Kushal Bhalaik
+ *
+ */
+
+public class TestContext {
+	public ScenarioContext scenarioContext;
+	private static ConfigReader configReader;
+	private LogUtils logUtils;
+	private RestTemplate restTemplate;
+	
+	public TestContext() {
+		scenarioContext = new ScenarioContext();
+		logUtils = LogUtils.getInstance(TestContext.class);
+		configReader = ConfigReader.getInstance();
+		restTemplate = RestTemplate.getInstance();
+
+	}
+
+	public ScenarioContext getScenarioContext() {
+		return scenarioContext;
+	}
+	
+	public ConfigReader getConfigReader() {
+		return configReader;
+	}
+
+
+	public LogUtils getLogUtils() {
+		return logUtils;
+	}
+	
+	public RestTemplate getRestTemplate() {
+		return restTemplate;
+	}
+
+}
